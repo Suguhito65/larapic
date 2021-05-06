b9d93b39a0b1382d26d225d9187898f290fce110
Suguhito <69892326+Suguhito65@users.noreply.github.com>
2021-05-06 21:03:56 +0900

    画像アップロードを本番環境に

---
+++ .history/laravel/resources/views/posts/search.blade_20210506204729.php
@@ -0,0 +1,11 @@
+<form action="{{ route('posts.search') }}" method="get" class="input-group mb-5" style="width: 60%; margin: 0 auto">
+    {{ csrf_field() }}
+    <input type="text" placeholder="search" name="search" value="" class="form-control">
+    <button type="submit" class="btn ml-1" style=": #ffa500">
+        <i class="fas fa-search"></i>
+    </button>
+</form>
+
+@isset($search_result)
+    <h2 class="text-center mb-3">{{ $search_result}}</h2>
+@endisset
---
+++ .history/laravel/resources/views/posts/search.blade_20210506204841.php
@@ -0,0 +1,11 @@
+<form action="{{ route('posts.search') }}" method="get" class="input-group mb-5" style="width: 60%; margin: 0 auto">
+    {{ csrf_field() }}
+    <input type="text" placeholder="search" name="search" value="" class="form-control">
+    <button type="submit" class="btn btn-outline-primary ml-1">
+        <i class="fas fa-search"></i>
+    </button>
+</form>
+
+@isset($search_result)
+    <h2 class="text-center mb-3">{{ $search_result}}</h2>
+@endisset
