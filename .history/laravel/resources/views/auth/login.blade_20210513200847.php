309131199da529808474ad3269cde960afe08555
Suguhito <69892326+Suguhito65@users.noreply.github.com>
2021-05-14 11:09:25 +0900

    デザイン変更

---
+++ .history/laravel/resources/views/auth/login.blade_20210513200847.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('title', 'ログイン')
+@section('content')
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        @include('layouts.error')
+        <div class="card" style="background: #fafafa">
+            <h3 class="card-header bg-dark text-white text-center" style="font-size: 1.5em">{{ __('ログイン') }}</div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email" class="">{{ __('メールアドレス') }}</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="@が必須です">
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="">{{ __('パスワード') }}</label>
+                        <input id="password" type="password" class="form-control" name="password" autocomplete="current-password" placeholder="6文字以上です">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary px-4 mt-4" style="border-radius: 1.2em">
+                            {{ __('ログイン') }}
+                        </button>
+                    </div>
+                </form>
+                <a class="nav-link text-right" href="{{ route('register') }}">{{ __('新規登録') }}</a>
+                <div class="text-center">
+                    <a href="/login/google" class="btn text-white" role="button" style="background: #ff4500; border-radius: 1.2em">
+                        <i class="fab fa-google"></i>　Google ログイン
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ .history/laravel/resources/views/auth/register.blade_20210513200832.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title', '新規登録')
+@section('content')
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        @include('layouts.error')
+        <div class="card" style="background: #fafafa">
+            <div class="card-header bg-dark text-white text-center">{{ __('新規登録') }}</h3>
+            <div class="card-body">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name" class="">{{ __('名前') }}</label>
+                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="">{{ __('メールアドレス') }}</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="@が必須です">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">{{ __('パスワード') }}</label>
+                        <input id="password" type="password" class="form-control" name="password" autocomplete="new-password" placeholder="6文字以上です">
+                    </div>
+                    <div class="form-group">
+                        <label for="password-confirm" class="">{{ __('パスワード再入力') }}</label>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary px-4 mt-4" style="border-radius: 1.2em">
+                            {{ __('新規登録') }}
+                        </button>
+                    </div>
+                </form>
+                <a class="nav-link text-right" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                <div class="text-center">
+                    <a href="/login/google" class="btn text-white px-4" role="button" style="background: #ff4500; border-radius: 1.2em">
+                    <i class="fab fa-google"></i>　Google ログイン
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
